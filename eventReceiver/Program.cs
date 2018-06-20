f08c544653a225f58cda61741080b7be0ebd60ed
Hayk <haykhairapetyan@gmail.com>
2018-06-20 15:36:34 +0000

    initial

---
+++ eventReceiver/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+using System.Threading.Tasks;
+
+
+namespace eventReceiver
+{
+    public class Program
+    {
+        private const string EhConnectionString = "Endpoint=sb://rating.servicebus.windows.net/;SharedAccessKeyName=default;SharedAccessKey=pnTREAwnbvmnoR5SdFo0Nsd7jkJoW9Abm5b6xrOqeVg=;";
+        private const string EhEntityPath = "immidiaterating";
+        private const string StorageContainerName = "host-49e555d3-dcbe-4ab0-a6f9-fced2fcbca77";
+        private const string StorageAccountName = "ratingstorage";
+        private const string StorageAccountKey = "DoragUmhI785UUOYoDCO+1n5C0mwmh37WPZFSztzcD5ENqlfF28Hyk+F5ujAVAolUxbMcmD49XRBkxRuXQlxyQ==";
+
+        private static readonly string StorageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", StorageAccountName, StorageAccountKey);
+
+        public static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            Console.WriteLine("Registering EventProcessor...");
+
+            var eventProcessorHost = new EventProcessorHost(
+                EhEntityPath,
+                PartitionReceiver.DefaultConsumerGroupName,
+                EhConnectionString,
+                StorageConnectionString,
+                StorageContainerName);
+
+            // Registers the Event Processor Host and starts receiving messages
+            await eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>();
+
+            Console.WriteLine("Receiving. Press ENTER to stop worker.");
+            Console.ReadLine();
+
+            // Disposes of the Event Processor Host
+            await eventProcessorHost.UnregisterEventProcessorAsync();
+        }
+    }
+}
---
+++ eventReceiver/SimpleEventProcessor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace eventReceiver
+{
+    public struct Message
+    {
+        int orderId;
+        string eventText;
+
+        public Message(int orderId, string eventText)
+        {
+            this.orderId = orderId;
+            this.eventText = eventText;
+        }
+
+    }
+
+    public class SimpleEventProcessor : IEventProcessor
+    {
+        public Task CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            Console.WriteLine($"Processor Shutting Down. Partition '{context.PartitionId}', Reason: '{reason}'.");
+            return Task.CompletedTask;
+        }
+
+        public Task OpenAsync(PartitionContext context)
+        {
+            Console.WriteLine($"SimpleEventProcessor initialized. Partition: '{context.PartitionId}'");
+            return Task.CompletedTask;
+        }
+
+        public Task ProcessErrorAsync(PartitionContext context, Exception error)
+        {
+            Console.WriteLine($"Error on Partition: {context.PartitionId}, Error: {error.Message}");
+            return Task.CompletedTask;
+        }
+
+       
+        public Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            foreach (var eventData in messages)
+            {
+                Console.WriteLine("aaaaa");
+                //var data = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
+                
+                var jsonBody = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
+                //var eventTypeName = (string)eventData.Properties["EventType"];
+                //var eventType = Type.GetType(eventTypeName);
+                var data1 = JsonConvert.DeserializeObject(jsonBody);
+                JObject data = JObject.Parse(jsonBody);
+                var a = data["orderId"];
+                //var data = JsonConvert.DeserializeObject(jsonBody, eventType);
+
+                Console.WriteLine($"Message received. Partition: '{context.PartitionId}' and '{eventData.Body};, Data: '{data}'"); 
+              
+                context.CheckpointAsync();
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
